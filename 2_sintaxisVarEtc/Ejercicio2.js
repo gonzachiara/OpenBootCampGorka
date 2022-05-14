45b0ab06ed7c4193aa072da5ad3eed018a90b4f7
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-05-13 23:52:54 -0300

    Ejercicio N°2
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    
    - Tu nombre (string)
    
    - Tu edad (number)
    
    - ¿Eres desarrollador? (boolean)
    
    - Tu fecha de nacimiento (Date)
    
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)

---
+++ 2_sintaxisVarEtc/Ejercicio2.js
@@ -0,0 +1,26 @@
+// - Tu nombre (string)
+
+var nombre = "gonzalo chiaravalle";
+
+// - Tu edad (number)
+
+var edad = 23
+
+// - ¿Eres desarrollador? (boolean)
+
+var dev = true;
+
+// - Tu fecha de nacimiento (Date)
+
+var fechaNacimiento = new Date(1998, 8, 04)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+var libroFav = {
+    titulo: "el principito",
+    autor: "señor x",
+    fecha: 1810,
+    url: "www.instagram.com/gonzachiara"
+}
+
+console.log(fechaNacimiento);

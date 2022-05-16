251a6ad8e44cf77e47e1c54dc22712ea4f7b2aa6
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-05-16 11:52:22 -0300

    Update Ejercicio3.js

--- 3_Bucles/Ejercicio3.js
+++ 3_Bucles/Ejercicio3.js
@@ -5,6 +5,11 @@
 for (i = 0; i < 10; i++) {
     console.log(10*i)
 }
+MAL
+let factorial = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
+}
 */
 // - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
 /*
@@ -14,11 +19,24 @@ while (i<max) {
     console.log(i*10)
     i++;
 }
+MAL
+   
+let factorial = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial) // Factorial de 10 = 3628800
+console.log(factorial) // Factorial de 10 = 3628800
 */
 // - factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
-let i = 0;
-let max = 10;
-while (i < max) {
-    
+let factorial = 1
+let num = 10
+while (true) {
+    factorial *= num
+    num--
+    if (num === 1) break
 }
+console.log(factorial) // Factorial de 10 = 3628800
 

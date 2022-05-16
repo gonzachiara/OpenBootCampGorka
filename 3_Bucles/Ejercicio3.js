a0e29087075799fb1a8ecb14aca569e2a1835f79
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-05-16 11:49:20 -0300

    Ejercicio 3 Bucles

---
+++ 3_Bucles/Ejercicio3.js
@@ -0,0 +1,24 @@
+// Crea los siguientes archivos JS:
+
+// - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+/*
+for (i = 0; i < 10; i++) {
+    console.log(10*i)
+}
+*/
+// - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+/*
+let i = 0;
+let max = 10;
+while (i<max) {
+    console.log(i*10)
+    i++;
+}
+*/
+// - factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+let i = 0;
+let max = 10;
+while (i < max) {
+    
+}
+

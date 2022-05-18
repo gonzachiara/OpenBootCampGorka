e52ac05a8fe599af219bc6df09f0438ceedc52cb
gonzachiara <97534308+gonzachiara@users.noreply.github.com>
2022-05-18 15:18:09 -0300

    Ejercicio 10 Modulos

---
+++ 10_Modulos/index.js
@@ -0,0 +1,11 @@
+import { suma, multiplica} from './modulo/controller.js';
+
+const sum = suma(4, 5);
+console.log(sum);
+
+const mult = multiplica(4, 5);
+console.log(mult);
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Hello world!'));
